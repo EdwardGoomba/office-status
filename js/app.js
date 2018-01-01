0f0904361535473fb4469701ff9f4594f211c8a6
EdwardGoomba <ask@edwardvdesign.com>
2018-01-01 13:01:27 -0500

    Finish ajax request

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if( xhr.readyState === 4 ) {
-      console.log(xhr.responseText);
+    var employees = JSON.parse(xhr.responseText);
+    var statusHTML = '<ul class="bulleted">';
+    for (var i=0; i<employees.length; i++) {
+      if (employees[i].inoffice === true) {
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+     statusHTML += employees[i].name;
+     statusHTML += '</li>';
+    }
+   statusHTML += '</ul>';
+   document.getElementById('employeeList').innerHTML = statusHTML;
   }
 
 };

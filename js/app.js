1da99753cacc53edfc566d01c1aa914992f25d2a
EdwardGoomba <ask@edwardvdesign.com>
2018-01-02 08:17:58 -0500

    Added jQuery for ajax request

--- js/app.js
+++ js/app.js
@@ -40,5 +40,26 @@ meetSpaces.onreadystatechange = function() {
   }
 };
 
-meetSpaces.open('GET', '../data/rooms.json');
-meetSpaces.send();
+// The following uses jQuery for the AJAX - uncomment and replace above code as needed
+
+// meetSpaces.open('GET', '../data/rooms.json');
+// meetSpaces.send();
+//
+// $(document).ready(function () {
+//   var url = "../data/employees.json";
+//
+//   $.getJSON(url, function (response) {
+//     var statusHTML = '<ul class="bulleted">';
+//     $.each(response, function (index, employee) {
+//       if (employee.inoffice === true) {
+//         statusHTML += '<li class="in">';
+//       } else {
+//         statusHTML += '<li class="out">';
+//       }
+//       statusHTML += employee.name + '</li>';
+//     });
+//     statusHTML += '</ul>';
+//     $('#employeeList').html(statusHTML);
+//   }); //end getJSON
+//
+// }); // end ready
